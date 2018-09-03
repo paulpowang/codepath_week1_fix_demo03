f1378d3c2ebd79c80e3934214c3846a38e967f62
paul <paul@pauls-MacBook-Pro.local>
2018-09-03 13:19:28 -0700

    Table View and Data Complete

---
+++ fix_demo_03/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  fix_demo_03
+//
+//  Created by paul on 9/3/18.
+//  Copyright © 2018 PoHung Wang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- fix_demo_03/NowPlayingViewController.swift
+++ fix_demo_03/NowPlayingViewController.swift
@@ -12,6 +12,8 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var movies: [[String:Any]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,10 +30,9 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
             }else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String:Any]]
-                for movie in movies{
-                    let title = movie["title"] as! String
-                    print(title)
-                }
+                self.movies = movies
+                self.tableView.reloadData()
+                
             }
         }
         task.resume()
@@ -39,11 +40,19 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        
         return cell
     }
 
