8f56c1027286a072ff693d2bdc069ba5008ef6e0
paul <paul@pauls-MacBook-Pro.local>
2018-09-08 14:56:42 -0700

    image view in superhero tab complete

---
+++ fix_demo_03/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  fix_demo_03
+//
+//  Created by paul on 9/8/18.
+//  Copyright © 2018 PoHung Wang. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
