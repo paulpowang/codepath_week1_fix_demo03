f3dd7cd35fe168ceb6e40e7e0eeabf42d92341c4
paul <paul@pauls-MacBook-Pro.local>
2018-09-08 15:36:54 -0700

    Require Story Complete

--- fix_demo_03/ViewControllers/SuperheroViewController.swift
+++ fix_demo_03/ViewControllers/SuperheroViewController.swift
@@ -23,6 +23,15 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
         // Do any additional setup after loading the view.
         
         collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = layout.minimumInteritemSpacing
+        let cellsPerLine: CGFloat = 2
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / (cellsPerLine - 1)
+        layout.itemSize = CGSize(width: width, height: width * 3/2)
+        
         fectchMovies()
     }
     
